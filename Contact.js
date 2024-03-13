95c2d720d5e1bdf4fe43ba01b20b57ff481286c8
Tariq ali <76946087+Tariq-the-Coder@users.noreply.github.com>
2024-03-13 11:33:38 +0530

    Update Contact.js

--- Contact.js
+++ Contact.js
@@ -1,12 +1,40 @@
-import React from 'react'
+import React from "react";
 
 const Contact = () => {
   return (
     <div>
-    This is contact us page!
-      
-    </div>
-  )
-}
+      <div className="contact-container">
+        <div className="message-box">
+          <iframe
+            src="https://forms.gle/rS7YqizrhWPszGG38"
+            width={"400"}
+            height={"1200"}
+            frameborder="0"
+          ></iframe>
+        </div>
 
-export default Contact
+        <div className="address-box">
+          <h1>Contact us</h1>
+          <div>
+            <b>Address:</b> Aiwan-e-Khidmat Near Bahishti Maqbara Mohalla
+            Ahmadiyya Qadian.
+          </div>
+          <div>
+            <b>Distt.:</b> Gurdaspur Punjab.
+          </div>
+          <div>
+            <b>Pincode:</b> 143516
+          </div>
+          <div>
+            <b>Phone:</b> 01872222900
+          </div>
+          <div>
+            <b>Email:</b> accedu@gmail.com
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
